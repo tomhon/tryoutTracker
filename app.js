8651b6ea7faad309ded6450e023bb7058d9769dd
Tom Honeybone <tomhon@hotmail.com>
2018-02-18 00:18:30 -0800

    added app settings

--- app.js
+++ app.js
@@ -29,7 +29,8 @@ var tableStorage = new azure.AzureBotStorage({gzipData: false}, azureTableClient
 
 var connector = new builder.ChatConnector({
     appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
+    appPassword: process.env.MICROSOFT_APP_PASSWORD,
+    openIdMetadata: process.env.BotOpenIdMetadata
 });
 
 
